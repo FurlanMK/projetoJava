da5d10d4977f1531e9885fd016ae52b9fc858010
FurlanMK <pedfurlan@hotmail.com>
2021-06-05 18:59:56 -0300

    código atualizado

--- src/main/inicio.java
+++ src/main/inicio.java
@@ -5,6 +5,11 @@ public class inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		int a = 2;
+		int b = 3;
+		int c = a*b;
+		
+		System.out.println(c);
 	}
 
 }
